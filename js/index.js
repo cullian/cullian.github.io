dac4e54d5eb522f098c689b06434d3fa6ee4885b
William Cullian <cullian@msn.com>
2017-05-11 10:09:39 -0700

    adding image files

--- js/index.js
+++ js/index.js
@@ -1,17 +1,17 @@
 // background images
-$(function() {
-  var backgroundImage = [
-    "https://static.pexels.com/photos/235621/pexels-photo-235621.jpeg",
-    "https://upload.wikimedia.org/wikipedia/commons/0/0d/Black_Labrador_Retrievers_portrait.jpg",
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Seljalandsfoss%2C_Su%C3%B0urland%2C_Islandia%2C_2014-08-16%2C_DD_201-203_HDR.JPG/1280px-Seljalandsfoss%2C_Su%C3%B0urland%2C_Islandia%2C_2014-08-16%2C_DD_201-203_HDR.JPG",
-    "https://images.pexels.com/photos/62627/niagara-cases-water-waterfall-62627.jpeg?w=940&h=650&auto=compress&cs=tinysrgb",
-    "https://images.pexels.com/photos/306803/pexels-photo-306803.jpeg?w=940&h=650&auto=compress&cs=tinysrgb",
-    "https://images.pexels.com/photos/50594/sea-bay-waterfront-beach-50594.jpeg?w=940&h=650&auto=compress&cs=tinysrgb",
-    "https://static.pexels.com/photos/139575/pexels-photo-139575.jpeg"
-  ];
-  // you can change the image here
-  $("body").css("background-image", "url(" + backgroundImage[6] + ")");
-});
+// $(function() {
+//   var backgroundImage = [
+//     "https://static.pexels.com/photos/235621/pexels-photo-235621.jpeg",
+//     "https://upload.wikimedia.org/wikipedia/commons/0/0d/Black_Labrador_Retrievers_portrait.jpg",
+//     "https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Seljalandsfoss%2C_Su%C3%B0urland%2C_Islandia%2C_2014-08-16%2C_DD_201-203_HDR.JPG/1280px-Seljalandsfoss%2C_Su%C3%B0urland%2C_Islandia%2C_2014-08-16%2C_DD_201-203_HDR.JPG",
+//     "https://images.pexels.com/photos/62627/niagara-cases-water-waterfall-62627.jpeg?w=940&h=650&auto=compress&cs=tinysrgb",
+//     "https://images.pexels.com/photos/306803/pexels-photo-306803.jpeg?w=940&h=650&auto=compress&cs=tinysrgb",
+//     "https://images.pexels.com/photos/50594/sea-bay-waterfront-beach-50594.jpeg?w=940&h=650&auto=compress&cs=tinysrgb",
+//     "https://static.pexels.com/photos/139575/pexels-photo-139575.jpeg"
+//   ];
+//   // you can change the image here
+//   $("body").css("background-image", "url(" + backgroundImage[6] + ")");
+// });
 
 // scroll spy
 $("body").scrollspy({ target: ".navbar", offset: 80 });
